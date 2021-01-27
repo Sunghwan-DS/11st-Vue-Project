413a9f81ae37d17389acb7b43de49ac37d3597fd
Sunghwan-DS <jeonsung02@gmail.com>
2021-01-27 12:46:02 +0900

    feat: Add Feature - RNB

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <l-n-b></l-n-b>
+    <r-n-b></r-n-b>
     <my-header></my-header>
     <billboard></billboard>
     <direct></direct>
@@ -9,12 +10,14 @@
 
 <script>
 import LNB from '~/src/components/LNB'
+import RNB from '~/src/components/RNB'
 import MyHeader from '~/src/components/MyHeader'
 import Billboard from '~/src/components/Billboard'
 import Direct from '~/src/components/Direct'
 export default {
   components: {
     LNB,
+    RNB,
     MyHeader,
     Billboard,
     Direct
